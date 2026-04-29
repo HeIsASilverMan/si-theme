15ca479dda7c258dc5cb50c299603fd3380ef2e3
HeIsASilverMan <shaneivers@gmail.com>
2026-04-29 15:08:32 +0100

    fix: only render page title on prose pages, not shortcode pages

--- page.php
+++ page.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 <div class="entry-content">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php if ( in_array( 'si-prose-page', get_body_class() ) ) : ?>
     <h1 class="entry-title"><?php the_title(); ?></h1>
-    <?php
-        the_content();
-    endwhile; endif; ?>
+    <?php endif; ?>
+    <?php the_content(); ?>
+    <?php endwhile; endif; ?>
 </div>
 <?php get_footer(); ?>
